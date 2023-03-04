0da98ceee39108b1aed0ebecbee4e5e9070efddf
mayumecristine <mayumecristine@gmail.com>
2023-03-04 19:33:49 -0300

    fix: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/xp42-omma/' : '/',
   outputDir: 'dist',
   pages: {
     index: {
       entry: 'src/main.js',
-      template: 'index.html',
+      template: '/index.html',
       filename: 'index.html'
     }
   }
