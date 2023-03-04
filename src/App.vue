8de68a9a90bc3a10bc86303d71aabc7873df3d56
mayumecristine <mayumecristine@gmail.com>
2023-03-04 20:25:58 -0300

    fix: vue.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
-<script setup>
+<script>
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import Home from '@/views/Home.vue'
+
+export default {
+  components: {
+    Header,
+    Home,
+    Footer,
+  }
+}
 </script>
 
 <template>
-  <Header id="Header"/>
+  <Header />
 
-  <Home />
+  <router-view />
 
   <Footer />
 </template>
--- src/router.js
+++ src/router.js
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
     },
+    {
+      path: '/:pathMatch(.*)*',
+      component: Home,
+    }
   ]
 })
 
